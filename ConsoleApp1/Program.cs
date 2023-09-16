29f28e04333947722e1398e32675c1a58d54bd88
pibo0121042 <pibo0121042@CL329-01>
2023-09-16 16:58:37 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Begin3()
+        {
+            Console.Write("Введите сторону а: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите сторону b: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int P = 2 * (a + b);
+            int S = a * b;
+            Console.WriteLine($"S = {S} P = {P}"); 
+
+
+        }
+        static void Main(string[] args)
+        {
+            Begin3();
+
+            Console.ReadKey();
+        }
+    }
+}
